2617ac0b780c11646f32277fb2229b1e57efd0b6
sergii-balan <sergii0balan@gmail.com>
2024-01-16 17:19:05 +0200

    Some test data added

--- src/test/java/com/sb/transposition/TranspositionApplicationTests.java
+++ src/test/java/com/sb/transposition/TranspositionApplicationTests.java
@@ -11,7 +11,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest(args = {"0", "[[0,1]]"})
+@SpringBootTest(args = {"-3",
+		"[[2,1],[2,6],[2,1],[2,8],[2,1],[2,9],[2,1],[2,6],[2,1],[2,8],[2,1],[2,9],[2,1],[2,11],[2,1],"
+		+ "[2,8],[2,1],[2,9],[2,1],[2,11],[2,1],[3,1],[2,1],[2,9],[2,1],[2,11],[2,1],[3,1],[2,1],[3,2],"
+		+ "[2,1],[2,11],[2,1],[3,1],[2,1],[2,9],[2,1],[2,11],[2,1],[2,8],[2,1],[2,9],[2,1],[2,6],[2,1],"
+		+ "[2,8],[2,1],[2,5],[2,1],[2,6],[2,1],[2,1],[2,1],[2,2],[2,1],[1,11],[2,1],[2,1],[2,1],[1,9],"
+		+ "[2,1],[1,11],[2,1],[1,8],[2,1],[1,9],[2,1],[1,6],[2,1],[1,11],[2,1],[1,8],[2,1],[1,9],[2,1],"
+		+ "[1,6],[2,1],[1,8],[2,1],[1,5],[2,1],[1,6]]"})
 @ActiveProfiles("test")
 @TestPropertySource(locations = "classpath:application.properties")
 class TranspositionApplicationTests {
